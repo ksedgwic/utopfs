c6cd2b0ef251d94d1b0459abb6710b8ddd5a37f3
ken.sedgwick <ken@bonsai.com>
2009-05-23 16:17:51 -0700

    Initialized INode fields on construction. Fixed names of INode fields.

--- modules/UTFileSystem/FileNode.cpp
+++ modules/UTFileSystem/FileNode.cpp
@@ -1,3 +1,7 @@
+#include <grp.h>
+#include <pwd.h>
+#include <sys/types.h>
+
 #include "Log.h"
 #include "Except.h"
 
@@ -10,13 +14,57 @@
 using namespace std;
 using namespace utp;
 
+namespace {
+
+string myuname()
+{
+    struct passwd pw;
+    struct passwd * pwp;
+    char buf[1024];
+    int rv = getpwuid_r(getuid(), &pw, buf, sizeof(buf), &pwp);
+    if (rv)
+        throwstream(InternalError, FILELINE
+                    << "getpwuid_r failed: " << strerror(rv));
+    if (!pwp)
+        throwstream(InternalError, FILELINE << "no password entry found");
+
+    return pwp->pw_name;
+}
+
+string mygname()
+{
+    struct group gr;
+    struct group * grp;
+    char buf[1024];
+    int rv = getgrgid_r(getgid(), &gr, buf, sizeof(buf), &grp);
+    if (rv)
+        throwstream(InternalError, FILELINE
+                    << "getgrgid_r failed: " << strerror(rv));
+    if (!grp)
+        throwstream(InternalError, FILELINE << "no group entry found");
+
+    return grp->gr_name;
+}
+
+} // end namespace
+
 namespace UTFS {
 
 FileNode::FileNode()
 {
     LOG(lgr, 4, "CTOR");
 
+    T64 now = T64::now();
+
     Random::fill(m_initvec, sizeof(m_initvec));
+
+    m_inode.set_mode(S_IRUSR | S_IWUSR | S_IRGRP);
+    m_inode.set_uname(myuname());
+    m_inode.set_gname(mygname());
+    m_inode.set_size(0);
+    m_inode.set_atime(now.usec());
+    m_inode.set_mtime(now.usec());
+    m_inode.set_ctime(now.usec());
 }
 
 FileNode::~FileNode()
