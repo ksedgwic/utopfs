8864e4cec0dcd7349ca3d191e38367ea451ec8b7
Igor Morozov <igmorv@gmail.com>
2009-08-31 00:46:44 -0700

    fixed tab size

--- config/WIN32/define.mk
+++ config/WIN32/define.mk
@@ -6,9 +6,9 @@ CURRDIRU =	$(shell pwd)
 CURRDIRW =	$(subst \,\\,$(shell cygpath -w $(shell pwd)))
 CURRDIRw =	$(wordlist 2, 2, $(subst :, ,$(CURRDIRW)))
 
-OBJEXT =		.obj
-SOEXT =			.dll
-EXEEXT =		.exe
+OBJEXT =	.obj
+SOEXT =		.dll
+EXEEXT =	.exe
 
 
 # It looks ugly enough but that is what MS does in order to get their compiler working in cmd shell. 
@@ -21,73 +21,70 @@ WINDIRW	= $(shell cygpath -Ww)
 # Visual Studio 2008 Setup
 # TODO: make it more general, support MS C++ Express Setup
 
-VSTUDIO	 			= 	$(subst \Common7\IDE,,$(shell regtool get "\HKLM\Software\Microsoft\VisualStudio\9.0\InstallDir"))
+VSTUDIO = $(subst \Common7\IDE,,$(shell regtool get "\HKLM\Software\Microsoft\VisualStudio\9.0\InstallDir"))
 
 # if not defined look for VS 2005
 ifndef VSTUDIO
-VSTUDIO	 			= 	$(subst \Common7\IDE,,$(shell regtool get "\HKLM\Software\Microsoft\VisualStudio\8.0\InstallDir"))
+VSTUDIO	= $(subst \Common7\IDE,,$(shell regtool get "\HKLM\Software\Microsoft\VisualStudio\8.0\InstallDir"))
 endif
 
 # if not defined look for VS .NET 2003
 ifndef VSTUDIO
-VSTUDIO	 			= 	$(subst \Common7\IDE,,$(shell regtool get "\HKLM\Software\Microsoft\VisualStudio\7.1\InstallDir"))
+VSTUDIO	= $(subst \Common7\IDE,,$(shell regtool get "\HKLM\Software\Microsoft\VisualStudio\7.1\InstallDir"))
 endif
 
 ifdef VSTUDIO
-VSINSTALLDIR		:=	$(VSTUDIO)
-VCINSTALLDIR		:=	$(VSTUDIO)\VC
-FrameworkDir		:=	$(WINDIRW)\Microsoft.NET\Framework
+VSINSTALLDIR 	:= $(VSTUDIO)
+VCINSTALLDIR 	:= $(VSTUDIO)\VC
+FrameworkDir 	:= $(WINDIRW)\Microsoft.NET\Framework
 
 # framework values are just taken from vs 2009, need to do more work to support earlier versions
-FrameworkVersion	:=	v2.0.50727
-Framework35Version	:=	v3.5
+FrameworkVersion := v2.0.50727
+Framework35Version := v3.5
 
-DevEnvDir			:=	$(VSTUDIO)Common7\IDE
+DevEnvDir	:= $(VSTUDIO)Common7\IDE
 
-CPPCMDPATH 			= 	$(shell cygpath $(subst \,\\,$(VSTUDIO)))
-VSTUDIOU 			= 	$(shell cygpath -m $(subst \,\\,$(VSTUDIO)))
+CPPCMDPATH	= $(shell cygpath $(subst \,\\,$(VSTUDIO)))
+VSTUDIOU 	= $(shell cygpath -m $(subst \,\\,$(VSTUDIO)))
 
-PLATFORM_SDKW	 	= 	$(shell regtool get "\HKCU\Software\Microsoft\Microsoft SDKs\Windows\CurrentInstallFolder")
+PLATFORM_SDKW	= $(shell regtool get "\HKCU\Software\Microsoft\Microsoft SDKs\Windows\CurrentInstallFolder")
 ifndef PLATFORM_SDKW
-PLATFORM_SDKW		= 	$(VSTUDIO)\PlatformSDK
+PLATFORM_SDKW	= $(VSTUDIO)\PlatformSDK
 endif
-PLATFORM_SDKU	 	= 	$(shell cygpath $(subst \,\\,$(PLATFORM_SDKW)))
-PLATFORM_SDKUW	 	= 	$(shell cygpath -m $(subst \,\\,$(PLATFORM_SDKW)))
+PLATFORM_SDKU	= $(shell cygpath $(subst \,\\,$(PLATFORM_SDKW)))
+PLATFORM_SDKUW	= $(shell cygpath -m $(subst \,\\,$(PLATFORM_SDKW)))
 
-PATH				:=	$(CPPCMDPATH)Common7/IDE:$(CPPCMDPATH)VC/bin:$(CPPCMDPATH)Common7/Tools:$(WINDIRU)/Microsoft.NET/Framework/v3.5:$(WINDIRU)/Microsoft.NET/Framework/v2.0.50727:$(CPPCMDPATH)VC/VCPackages:$(PLATFORM_SDKU)bin:${PATH}
+PATH		:= $(CPPCMDPATH)Common7/IDE:$(CPPCMDPATH)VC/bin:$(CPPCMDPATH)Common7/Tools:$(WINDIRU)/Microsoft.NET/Framework/v3.5:$(WINDIRU)/Microsoft.NET/Framework/v2.0.50727:$(CPPCMDPATH)VC/VCPackages:$(PLATFORM_SDKU)bin:${PATH}
 
-BASEINCLUDE			=	-I"$(VSTUDIO)VC\atlmfc\include" -I"$(VSTUDIO)VC/include" -I"$(PLATFORM_SDKW)include"
+BASEINCLUDE	= -I"$(VSTUDIO)VC\atlmfc\include" -I"$(VSTUDIO)VC/include" -I"$(PLATFORM_SDKW)include"
 
-BASELIB				:=	-L $(VSTUDIO)VC\ATLMFC\LIB -L$(VSTUDIO)VC\LIB -L$(PLATFORM_SDKW)LIB
+BASELIB		:= -L $(VSTUDIO)VC\ATLMFC\LIB -L$(VSTUDIO)VC\LIB -L$(PLATFORM_SDKW)LIB
 
-LIBPATH				:=	-LIBPATH:"$(WINDIRW)\Microsoft.NET\Framework\v3.5" \
-						-LIBPATH:"$(WINDIRW)\Microsoft.NET\Framework\v2.0.50727" \
-						-LIBPATH:"$(VSTUDIO)VC\ATLMFC\LIB" \
-						-LIBPATH:"$(VSTUDIO)VC\LIB" \
-						-LIBPATH:"$(PLATFORM_SDKW)LIB"
+LIBPATH 	:= 	-LIBPATH:"$(WINDIRW)\Microsoft.NET\Framework\v3.5" -LIBPATH:"$(WINDIRW)\Microsoft.NET\Framework\v2.0.50727" \
+			-LIBPATH:"$(VSTUDIO)VC\ATLMFC\LIB" -LIBPATH:"$(VSTUDIO)VC\LIB" -LIBPATH:"$(PLATFORM_SDKW)LIB"
 
-#MANIFEST_EMBED			= 1
-SOCMT					= mt
-SOCMTFLAGS				= -manifest
-SOCMTOUTPUTRESOURCE		= -outputresource
-SOC_MANIFEST_BASENAME   = __VC.Debug
+#MANIFEST_EMBED	= 1
+SOCMT			= mt
+SOCMTFLAGS		= -manifest
+SOCMTOUTPUTRESOURCE	= -outputresource
+SOC_MANIFEST_BASENAME	= __VC.Debug
 
 endif
 
-CPPCMD 				=  	cl
+CPPCMD = cl
 
-LIBCMD 				=   lib -OUT:$@
+LIBCMD = lib -OUT:$@
 
-LINK  				=   link
-SOEXT 				=	.dll
-SOCMD 				=	$(LINK)
+LINK  = link
+SOEXT = .dll
+SOCMD = $(LINK)
 
-LDCMD 				= 	link
+LDCMD = link
 
 ifeq ($(BUILD),DEBUG)
-CPPFLAGS =		-Od -Z7 -MDd -W3 -nologo -GF -GR -GT -EHsc
+CPPFLAGS = -Od -Z7 -MDd -W3 -nologo -GF -GR -GT -EHsc
 else
-CPPFLAGS =		-O2 -Ot -Z7 -MD -W3 -nologo -GF -GR -GT -G7 -EHsc -arch:SSE2 
+CPPFLAGS = -O2 -Ot -Z7 -MD -W3 -nologo -GF -GR -GT -G7 -EHsc -arch:SSE2 
 endif
 
 #RMFILESCMD = del /Q
@@ -96,22 +93,22 @@ RMDIRSFLAGS = 	/S /Q
 
 
 ifeq ($(BUILD),DEBUG)
-LDFLAGS =		$(LIBPATH) -nologo -DEBUG -DEBUGTYPE:CV -map
+LDFLAGS = $(LIBPATH) -nologo -DEBUG -DEBUGTYPE:CV -map
 else
-LDFLAGS =		$(LIBPATH) -nologo -map
+LDFLAGS = $(LIBPATH) -nologo -map
 endif
 
 ifeq ($(BUILD),DEBUG)
-SOFLAGS =		$(LIBPATH) -nologo -DLL -SUBSYSTEM:WINDOWS -DEBUG \
-				-DEBUGTYPE:CV -MAP
+SOFLAGS = $(LIBPATH) -nologo -DLL -SUBSYSTEM:WINDOWS -DEBUG \
+			-DEBUGTYPE:CV -MAP
 else
-SOFLAGS =		$(LIBPATH) -nologo -DLL -SUBSYSTEM:WINDOWS -INCREMENTAL:NO -LARGEADDRESSAWARE -TSAWARE:NO -OPT:REF -OPT:ICF /OPT:NOWIN98 -MACHINE:X86 
+SOFLAGS = $(LIBPATH) -nologo -DLL -SUBSYSTEM:WINDOWS -INCREMENTAL:NO -LARGEADDRESSAWARE -TSAWARE:NO -OPT:REF -OPT:ICF /OPT:NOWIN98 -MACHINE:X86 
 endif
 
-INSTFILECMD = 	install -D
-INSTDIRCMD =	install -d
+INSTFILECMD = install -D
+INSTDIRCMD 	= install -d
 
-SRCENVSH =		env.sh
+SRCENVSH =	env.sh
 
 ifeq ($(BUILD),DEBUG)
 DEFS +=			-DDEBUG
